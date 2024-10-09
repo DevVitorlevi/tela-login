824804fc20693388e2263b38ff87a7f43a093b3a
Dev Vitor <160429477+DevVitorlevi@users.noreply.github.com>
2024-10-09 20:46:14 -0300

    att

--- script.js
+++ script.js
@@ -3,8 +3,20 @@ const form = document.getElementById('form')
 const inputs = document.querySelectorAll('.required')
 const spans = document.querySelectorAll('.span-required')
 
+document.addEventListener('keydown',(evt)=>{
+    if(evt.key=='Enter'){
+        impedirEnvio(evt)
+    }
+})
+form.addEventListener('submit',impedirEnvio)
+function impedirEnvio(ev){
+    ev.preventDefault()//Impedir o Envio do Form
+    ValidarEmail()
+    ValidarSenha()
+    
+}
 function CasoError(indice){
-    inputs[indice].style.border= '2px solid #e63636'
+    inputs[indice].style.border= '1px solid #e63636'
     spans[indice].style.display='block'
 }
 function NotError(indice){
@@ -16,10 +28,17 @@ inputs[0].addEventListener('input',ValidarEmail)
 inputs[1].addEventListener('input',ValidarSenha)
 
 function ValidarEmail(){
-    if(emailRegex.test(inputs[0].length)){
-        CasoError(0)
-    }else{
+    if(emailRegex.test(inputs[0].value)){
         NotError(0)
     }
+    else{
+        CasoError(0)
+    }
 }
-
+function ValidarSenha(){
+    if((inputs[1].value.length<4 ||inputs[1].value.length>60 )){
+        CasoError(1)
+    }else{
+        NotError(1)
+    }
+}
